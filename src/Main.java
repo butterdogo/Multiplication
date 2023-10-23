485e21cea0d20e084e448fc35b065d2f02b378b0
butterdogo <olle.axelsson@elev.ga.ntig.se>
2023-10-23 14:15:25 +0200

    skrev kod: while och do-while

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,34 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Skift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
 
-            // Press Skift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        Scanner tgb = new Scanner(System.in);
+        int tal = tgb.nextInt();
+        tgb.nextLine();
+
+        /*
+        for (int i = 1; i <= 10; i++) {
+
+
+            System.out.println(tal * i);
         }
+    */
+    /*
+        int i = 1;
+        while (i <11)   {
+            System.out.println(i * tal);
+            i++;
+
+        }
+    */
+        int i = 1;
+        do {
+            System.out.println(i * tal);
+            i++;
+        }
+        while (i <11);
+        }
+
+
     }
-}
